2556826b94463624cb201e10977f0e385fa66b57
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-07-05 16:59:17 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wheather app
+
+project that fetch wheater data form a free API
+
+## Getting started
+
+clone project, install dependencies and run
+
+`npm run dev`
+
+## contributors
+
+* [Henry Tabima Giraldo](https://HenryTabima.github.io)
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    watchContentBase: true,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
